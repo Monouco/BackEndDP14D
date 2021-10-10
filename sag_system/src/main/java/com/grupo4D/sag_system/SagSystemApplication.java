7c9dd7073706f7e7e525cb1b552bc0055dba3c5e
Monouco <71278463+Monouco@users.noreply.github.com>
2021-10-10 17:02:20 -0500

    Add files via upload

---
+++ sag_system/src/main/java/com/grupo4D/sag_system/SagSystemApplication.java
@@ -0,0 +1,13 @@
+package com.grupo4D.sag_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagSystemApplication.class, args);
+	}
+
+}
---
+++ sag_system/src/test/java/com/grupo4D/sag_system/SagSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4D.sag_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
