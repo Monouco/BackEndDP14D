888dd66d1749295063591aaa05a630d7df7ffc0e
Monouco <algoalgo175@gmail.com>
2021-10-26 23:59:41 -0500

    Listar Bloqueos

--- sag_system/src/main/java/com/grupo4D/sag_system/repository/BloqueoRepository.java
+++ sag_system/src/main/java/com/grupo4D/sag_system/repository/BloqueoRepository.java
@@ -1,10 +1,21 @@
 package com.grupo4D.sag_system.repository;
 
 import com.grupo4D.sag_system.model.Bloqueo;
+import com.grupo4D.sag_system.model.RutaXNodo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 @Repository
 public interface BloqueoRepository extends CrudRepository<Bloqueo,Integer> {
+
+    @Query(
+            value = "SELECT * FROM bloqueo u WHERE u.activo = 1 " +
+                    "and fecha_fin > ?1 ",
+            nativeQuery = true)
+    public ArrayList<Bloqueo> listarPedidosActuales(LocalDateTime fecha);
 
 }
--- sag_system/src/main/java/com/grupo4D/sag_system/service/BloqueoService.java
+++ sag_system/src/main/java/com/grupo4D/sag_system/service/BloqueoService.java
@@ -1,16 +1,22 @@
 package com.grupo4D.sag_system.service;
 
 import com.grupo4D.sag_system.model.Bloqueo;
+import com.grupo4D.sag_system.model.Nodo;
 import com.grupo4D.sag_system.repository.BloqueoRepository;
+import com.grupo4D.sag_system.repository.NodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 @Service
 public class BloqueoService{
     @Autowired
     BloqueoRepository camionRepository;
+
+    @Autowired
+    NodoRepository nodoRepository;
 
     public Bloqueo guardarBloqueo(Bloqueo bloqueo){
         return camionRepository.save(bloqueo);
@@ -21,7 +27,14 @@ public class BloqueoService{
     }
 
     public ArrayList<Bloqueo> listarBloqueos() {
-        return (ArrayList<Bloqueo>) camionRepository.findAll();
+        ArrayList<Bloqueo> bloqueos = camionRepository.listarPedidosActuales(LocalDateTime.now());
+        int i;
+        for(i = 0; bloqueos.size()> i; i++){
+            Bloqueo bloqueo = bloqueos.get(i);
+            Nodo nodoBloqueado = nodoRepository.findNodoById(bloqueo.getNodo().getId());
+            bloqueo.setNodo(nodoBloqueado);
+        }
+        return bloqueos;
     }
 
 }
